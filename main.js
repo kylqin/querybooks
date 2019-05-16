50ec0a56f0c2291277e2cbfb3ca9a7b3f0a70e59
qinxij <qinxij@163.com>
2019-05-16 18:01:17 +0800

    add help info with -h, --help

--- main.js
+++ main.js
@@ -27,12 +27,27 @@ function runApp () {
   const ARGS = parseArgs()
   const booklist = createList()
 
+  if (ARGS.subCmd === '-h' || ARGS.subCmd === '--help') {
+    return showHelp()
+  }
   // const filtered = fuzzy.search(searchTerm, booklist);
   // const filtered = simple.search(searchTerm, booklist);
   // const filtered = lunr.search(searchTerm, booklist);
   const filtered = (subCmdTable[ARGS.subCmd] || subCmdTable.default).search(ARGS.searchTerm, booklist)
 
   pTerm.p(filtered)
+}
+
+function showHelp () {
+  console.log(`usage: bk [-h | --help]
+       bk {fuzzy | f | lunr | l | simple | s} searchTerm
+
+These are common \`bk\` sub commands used to search books with various search engines:
+
+   fuzzy,  f    Search with fuzzy style
+   lunr,   l    Search with lunr.js, a search engine is a bit like Solr
+   simple, s    Search with exact match case insensitively
+  `)
 }
 
 function parseArgs () {
